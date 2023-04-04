6b48b09434bd2c6a9929b819c4f440f2ed3fda49
drypnotic <104222938+josetipula@users.noreply.github.com>
2023-04-03 22:33:56 -0500

    Add files via upload

--- tarea/main.cpp
+++ tarea/main.cpp
@@ -6,8 +6,17 @@
 // 2. No se permite el uso de librerias adicionales.
 
 #include "foo.h"
-
 // Puede validar su codigo con la siguiente funcion main
 int main(){
+    Matriz2D m1;            // Matriz cuadrada de 3x3
+    Matriz2D m2(5);         // Matriz cuadrada de 5x5
+    Matriz2D m3(5,2);
+    Matriz2D m4 = m2;
+    Matriz2D m5 = move(m2);
 
+    Matriz2D m8 = m3 - 1.2;
+    Matriz2D m9 = m3 * 5;
+    Matriz2D m10 = m3 / 7;
+    cout << m3;
+    std::srand(time(nullptr));
 }
